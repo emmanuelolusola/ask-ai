ebaf442218d3234bb939e7ea931bca89a1b7f27b
emmanuelolusola <emmanuelolusolaoyeleke@gmail.com>
2023-01-21 17:19:59 +0100

    new commit

--- client/script.js
+++ client/script.js
@@ -81,7 +81,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("https://ask-ai-6xc5.onrender.com", {
+  const response = await fetch("http://localhost:5000", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",

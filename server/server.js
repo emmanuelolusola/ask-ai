9c16ca767ed1bfabe60a3babf347e9882a9d56ee
emmanuelolusola <emmanuelolusolaoyeleke@gmail.com>
2023-01-21 17:20:50 +0100

    new commit

--- server/server.js
+++ server/server.js
@@ -45,5 +45,5 @@ app.post("/", async (req, res) => {
 });
 
 app.listen(5000, () =>
-  console.log("AI server started on https://ask-ai-sv24.onrender.com")
+  console.log("AI server started on http://localhost:5000")
 );
